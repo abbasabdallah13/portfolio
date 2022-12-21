2416fe07a0d9b2826d604bd31614f3f70e882970
Abbas Abdallah <abbasab13@outlook.com>
2022-12-21 10:56:17 +0200

    updated

--- src/container/skills/Skills.jsx
+++ src/container/skills/Skills.jsx
@@ -22,11 +22,11 @@ const isInView = useInView(ref);
   return (
     <section style={{marginTop:'5rem', height:'fit-content', position:'relative'}} id='skills'>
       <div style={{position:'relative', top:'min(25px,4vw)', textAlign:'center', width:'100%', opacity: isInView?'1':0, transition:'1s', transform:!isInView?'translateX(-200px)':'translateX(0)'}} ref={ref}><img src={skillsTitle} alt='skills title' style={{ maxWidth:'70%'}} /></div>
-      <div style={{backgroundColor:'#37B7B3',paddingBottom:'10rem', display:'flex', justifyContent:'space-around', fontFamily:'Hepta Slab'}}>
+      <div style={{backgroundColor:'#202020',paddingBottom:'10rem', display:'flex', justifyContent:'space-around', fontFamily:'Hepta Slab'}}>
         <div className='skills-container' >
           <img src={nontechnicalSkills} alt='non technical skills' />
           <h4>Non-technical skills:</h4>
-          <p>Creative <span style={{color:'#f53083', fontWeight:'600'}}>problem-solver</span> with the ability to figure out the best approach to develop a design, to fixing up bugs that crop up, to figuring out how
+          <p>Creative <span style={{color:'#ffcf52', fontWeight:'600'}}>problem-solver</span> with the ability to figure out the best approach to develop a design, to fixing up bugs that crop up, to figuring out how
             to make the front end work with the backend.
             <br/><br/>Posses<span style={{color:'#ffcf52', fontWeight:'600'}}> verbal and vocal communication skills</span> necessary
             to communicate ideas with others. 
@@ -41,7 +41,7 @@ const isInView = useInView(ref);
           <p>
           I develop designs using the most optimized and neat code in my editor and have the necessary skills to test using various libraries.
           <br/><br/>PS. I sometimes get creative making up my own designs.
-          <br/><br/><span style={{color:'#222725', fontWeight:'600'}}>Actual Work Experience:</span> 18 months
+          <br/><br/><span style={{color:'#ffcf52', fontWeight:'600'}}>Actual Work Experience:</span> 18 months
           </p>
         </div>
 
